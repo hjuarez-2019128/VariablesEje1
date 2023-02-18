5ce0c1dbd291a973385bd641eca2037cecd2e725
AJuarez2023001 <HansAlexanderMerucry@gmail.com>
2023-02-18 12:20:01 -0600

    Mi primer Ejercicio en Python con variables

---
+++ Variables.py
@@ -0,0 +1,5 @@
+#UNA EMPRESA DESEA CONOCER LOS DATOS MAS IMPORTANTES DE LOS POSTULANTES 
+#Se necesita el nombre de la persona edad y numero telefonico
+Nombre Persona = 'Alexander'
+print(Nombre Persona)
+
